87225defd749f122a4cbe805c672c6585730a657
jjmachan <jamesjithin97@gmail.com>
2018-04-13 09:48:41 +0530

    changed date format

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
 var d = new Date();
+date = d.toISOString();
 var month = d.getMonth()+1;
-var date = d.getDate()+":"+month+":"+d.getFullYear();
-document.getElementById('date').innerHTML = date;
+//var date = d.getDate()+":"+month+":"+d.getFullYear();
+document.getElementById('date').innerHTML = date.slice(0,10);
